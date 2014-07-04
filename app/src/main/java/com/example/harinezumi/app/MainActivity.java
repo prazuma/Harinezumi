1ca9bf600e462e9f8a42181a90cb292586cac178
g12908hm+git@gm.tsuda.ac.jp <g12908hm+git@gm.tsuda.ac.jp>
2014-07-04 11:56:12 +0900

    add FinishApplication

--- app/src/main/java/com/example/harinezumi/app/MainActivity.java
+++ app/src/main/java/com/example/harinezumi/app/MainActivity.java
@@ -45,9 +45,9 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        int id=item.getItemId();
+        if(id==R.id.action_settings){
+            finish();
         }
         return super.onOptionsItemSelected(item);
     }
